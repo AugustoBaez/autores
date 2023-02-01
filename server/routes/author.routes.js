04f9b4bb8d1aca7e5726898e9232ec70a0c90806
Augusto Baez <augustobaez@iMac-de-abc-desarrollo.local>
2023-02-01 16:13:49 -0300

    change routes

--- server/routes/author.routes.js
+++ server/routes/author.routes.js
@@ -5,5 +5,5 @@ module.exports = (app) => {
   app.get("/api/author/:id", AuthorController.findAuthor);
   app.post("/api/author/new", AuthorController.addAuthor);
   app.put("/api/author/update/:id", AuthorController.updateAuthor);
-  app.delete("/api/author/:id", AuthorController.deleteAuthor);
+  app.delete("/api/author/delete/:id", AuthorController.deleteAuthor);
 };
