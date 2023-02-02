9646d516c39fa8d1ef912fb236b330b042eeda52
Augusto Baez <augustobaez@iMac-de-abc-desarrollo.local>
2023-02-02 17:38:47 -0300

    tratar de mostrar errores de mensaje

--- client/src/components/AddAuthor.jsx
+++ client/src/components/AddAuthor.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const AddAuthor = () => {
   const [name, setName] = useState("");
-  const [errors, setErrors] = useState({})
+  const [errors, setErrors] = useState("");
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -13,15 +13,20 @@ const AddAuthor = () => {
       })
       .then((res) => {
         console.log(res, "succesful");
-        navigate("/main");
+        /*         navigate("/main"); */
+        /*         setErrors(error.data.error.message); */
       })
       .catch((error) => {
         console.log(error, "error haciendo post");
-        setErrors(error.data.error.message)
+        /* const errorArr = [];
+        for (const key of Object.keys(errorResponse)) {
+          errorArr.push(errorResponse[key].message);
+        } */
+        setErrors(error.data.error.errors.message);
       });
   };
 
-  console.log(errors)
+  console.log(errors);
   return (
     <div>
       <h1>Favorite authors</h1>
@@ -32,7 +37,7 @@ const AddAuthor = () => {
       <form action="" onSubmit={handleSubmit}>
         <p>Name:</p>
         <input type="text" onChange={(e) => setName(e.target.value)} />
-        {errors.name ? <span > {errors.message}</span> : null }<br></br>
+        {<p>{errors}</p>}
         <button>
           <a href={`/main`}></a>
           Cancel
